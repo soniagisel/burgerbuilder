028bd41fb51847236c708942f15db8ccafac2868
Sonia Pepe <sogispe@gmail.com>
2020-05-18 16:52:43 -0300

    Reusing backdrop with sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
+
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import classes from './Layout.module.css'
 
-const layout = props => {
-  return (
-    <React.Fragment>
-      <Toolbar />
-      <SideDrawer />
-      <main className={classes['content']}>{props.children}</main>
-    </React.Fragment>
-  )
+class Layout extends Component {
+  state = {
+    showSideDrawer: true,
+  }
+
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false })
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Toolbar />
+        <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+        <main className={classes['content']}>{this.props.children}</main>
+      </React.Fragment>
+    )
+  }
 }
 
-export default layout
+export default Layout
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,18 +1,30 @@
+import Backdrop from '../../UI/Backdrop/Backdrop'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
 import React from 'react'
 import classes from './SideDrawer.module.css'
 
 const sideDrawer = props => {
+  console.log('PROPS OPEN', props.open)
+  let attachedClasses = [classes['SideDrawer'], classes['Close']]
+  //console.log('attached string ', attachedClasses.join(' '))
+  if (props.open) {
+    attachedClasses = [classes['SideDrawer'], classes['Open']]
+    //console.log('after open ', attachedClasses.join(' '))
+  }
+  console.log('attached string ', attachedClasses.join(' '))
   return (
-    <div className={classes['SideDrawer']}>
-      <div className={classes.Logo}>
-        <Logo />
+    <React.Fragment>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </React.Fragment>
   )
 }
 
