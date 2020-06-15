9c0680142ff449d6c708613e6a70659ebb96d970
Sonia Pepe <sogispe@gmail.com>
2020-06-15 19:18:00 -0300

    Added form and final burger status before checkout

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -88,25 +88,13 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    this.setState({ loading: true })
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Sonia',
-        address: {
-          street: '742 Evergreen Ave.',
-          zipCode: '765456',
-          country: 'United States',
-        },
-        email: 'test@burgertesting.com',
-      },
-      deliveryMethod: 'fastest',
+    const queryParams = []
+    for (let i in this.state.ingredients) {
+      queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
     }
-    axios
-      .post('/orders.json', order)
-      .then(res => this.setState({ loading: false, purchasing: false }))
-      .catch(err => this.setState({ loading: false, purchasing: false }))
+    queryParams.push('price=' + this.state.totalPrice)
+    const queryString = queryParams.join('&')
+    this.props.history.push({ pathname: '/checkout', search: '?' + queryString })
   }
 
   render() {
